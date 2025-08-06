71fbe0d341cf2642c8dce55fce1a9d0af61958a5
AIFCodes <149904742+aifontao@users.noreply.github.com>
2025-08-06 20:15:35 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from CS50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+#Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 library to use SQLite database
+db = SQL("sqlite:///travel.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    """Show user dashboard"""
+
+    return render_template(index.html)

2b03a3eddcf26d16725d02cb7254fe4935fba700
AIFCodes <149904742+aifontao@users.noreply.github.com>
2025-08-07 19:21:05 +0100

    Add apology from cs50 finance

--- helpers.py
+++ helpers.py
@@ -4,6 +4,31 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from functools import wraps
 
 
+def apology(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
 def login_required(f):
     """Decorator to ensure user is logged in"""
     
